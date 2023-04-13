49fe60d58e7adca692c06fbca40df6feadfe23ab
ha-yong <gkdydejr@gmail.com>
2023-04-14 08:38:14 +0900

    3day commit

---
+++ 4.4 Recap.ts
@@ -0,0 +1,40 @@
+// typeとinterfaceはobjectの形を教えてあげるためにつかう
+// 
+type Recap = {
+    name:string
+}
+type Recapp = Recap &{
+    lastName:string
+}
+const playerA: Recapp ={
+    name:"ha",
+    lastName:"kim"
+}
+
+interface Recap2 {
+    name:string
+}
+interface Recapp2 extends Recapp{
+    lastName:string
+}
+const playerB: Recapp2 ={
+    name:"ha",
+    lastName:"kim"
+}
+
+// typeとinterfaceの使いかたは一緒
+type Recap3 = {
+    firstName:string
+}
+
+interface Recapp3{
+    firstName:string
+}
+
+class RecapClass implements Recap3{
+    constructor(public firstName:string){}
+}
+
+class RecapClass2 implements Recapp3{
+    constructor(public firstName:string){}
+}
---
+++ 4.5 Polymorphism.ts
@@ -0,0 +1,27 @@
+// Genericsを使うことで、全てのタイプを使用できる。
+interface SStorage<T> {
+    [key: string]: T;
+}
+
+class LocalStorage<T> {
+    private storage:SStorage<T> ={}
+    set(key:string, value:T){
+        this.storage[key] = value
+    }
+    remove(key:string){
+        delete this.storage[key]
+    }
+    get(key:string):T{
+        return this.storage[key]
+    }
+    clear(){
+        this.storage = {}
+    }
+}
+
+const stringsStorage = new LocalStorage<string>()
+stringsStorage.get("key")
+stringsStorage.set("key", "value")
+const boolenasStorage = new LocalStorage<boolean>()
+boolenasStorage.get("key")
+boolenasStorage.set("key", true)
