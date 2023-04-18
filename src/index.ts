5174613966793daeffac90c1d55f4c86009d7305
ha-yong <gkdydejr@gmail.com>
2023-04-18 09:01:39 +0900

    6day commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-const hello = () => "hi"
+import crypto from 'crypto'; 
+
+interface BlockShape {
+    hash: string;
+    prevHash: string;
+    height: number;
+    data: string;
+}
+class Block implements BlockShape {
+    public hash:string;
+    constructor(
+        public prevHash: string,
+        public height: number,
+        public data: string
+    ) {
+        this.hash = Block.calcHash(this.prevHash, this.height, this.data);
+    }
+    static calcHash(prevHash: string, height: number, data: string) {
+        const toHash = '${prevHash}${height}${data}';
+    }
+}
